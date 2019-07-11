cfe8f81aba159552e6b9719bcac9cb9f7a90cae6
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-11 15:46:42 +0200

    aggiunto controllo dell'input

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 
- /*    Il software riceve in input un lungo testo e una serie di parole da censurare.
-    Restituisce il testo con xxx al posto delle parole censurate.
-    Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
- */
+/*    Il software riceve in input un lungo testo e una serie di parole da censurare.
+   Restituisce il testo con xxx al posto delle parole censurate.
+   Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
+*/
 
 var theText = prompt("inserisci testo da censurare");
 var theWords = prompt("inserisci parole da censurare, divise da spazi");
 
 console.log(theText);
 
-function censor(text, words){
+function censor(text, words) {
+  if (words && text) {
+    //trasformo il testo e le parole da censurare in array;
+    var textArray = text.split(" ");
+    var wordsArray = words.split(" ");
 
-  //trasformo il testo e le parole da censurare in array;
-  var textArray = text.split(" ");
-  var wordsArray = words.split(" ");
+    var badWordsSum = 0;
 
-  var badWordsSum = 0;
-
-  for (var i = 0; i < wordsArray.length; i++){
-    while (textArray.indexOf(wordsArray[i]) !== -1){
-      var index = textArray.indexOf(wordsArray[i]);
-      textArray[index] = "xxx";
-      badWordsSum += 1;
+    for (var i = 0; i < wordsArray.length; i++) {
+      while (textArray.indexOf(wordsArray[i]) !== -1) {
+        var index = textArray.indexOf(wordsArray[i]);
+        textArray[index] = "xxx";
+        badWordsSum += 1;
+      }
     }
+
+    var result = textArray.join(" ");
+
+    return ("Parole censurate: " + badWordsSum + " su " + textArray.length + "\n" + result);
+  } else {
+    return "errore nell'input";
   }
 
-  var result = textArray.join(" ");
 
-  return ("Parole censurate: " + badWordsSum + " su " + textArray.length + "\n" + result);
 }
 
 console.log(censor(theText, theWords));
