b5395799b59eb1148022410e1492845726c6b446
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-11 15:44:48 +0200

    aggiunta logica

---
+++ app.js
@@ -0,0 +1,33 @@
+
+ /*    Il software riceve in input un lungo testo e una serie di parole da censurare.
+    Restituisce il testo con xxx al posto delle parole censurate.
+    Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
+ */
+
+var theText = prompt("inserisci testo da censurare");
+var theWords = prompt("inserisci parole da censurare, divise da spazi");
+
+console.log(theText);
+
+function censor(text, words){
+
+  //trasformo il testo e le parole da censurare in array;
+  var textArray = text.split(" ");
+  var wordsArray = words.split(" ");
+
+  var badWordsSum = 0;
+
+  for (var i = 0; i < wordsArray.length; i++){
+    while (textArray.indexOf(wordsArray[i]) !== -1){
+      var index = textArray.indexOf(wordsArray[i]);
+      textArray[index] = "xxx";
+      badWordsSum += 1;
+    }
+  }
+
+  var result = textArray.join(" ");
+
+  return ("Parole censurate: " + badWordsSum + " su " + textArray.length + "\n" + result);
+}
+
+console.log(censor(theText, theWords));
